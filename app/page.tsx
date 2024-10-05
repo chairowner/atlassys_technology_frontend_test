2a9c0206f1e61a99135f3f94815ed3bd0c36bdf9
Danil Sagajdachnyj <chairowner@yandex.ru>
2024-10-05 22:30:08 +0500

    create

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,74 @@
 import Image from "next/image";
+import "./page.scss";
+import { FC, InputHTMLAttributes } from "react";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+	label: string;
+}
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+const Input: FC<InputProps> = ({ label, ...props }) => {
+	return (
+		<label className="form_input">
+			<span>{label}</span>
+			<input {...props} />
+		</label>
+	);
+};
+
+export default function Setting() {
+	return (
+		<>
+			<ul className="page__list">
+				<li className="selected">Edit profile</li>
+				<li>Preference</li>
+				<li>Security</li>
+			</ul>
+			<div className="page__content">
+				<div className="page__content__profile__wrapper">
+					<div className="page__content__profile__container">
+						<Image
+							className="page__content__profile__image"
+							src="/img/profile.png"
+							width={130}
+							height={130}
+							alt="Profile"
+						/>
+						<Image
+							className="page__content__profile__edit"
+							src={"/img/edit.png"}
+							width={30}
+							height={30}
+							alt="Edit"
+						/>
+					</div>
+				</div>
+				<div className="page__content__form">
+					<div className="page__content__form__fields">
+						<Input label="Your Name" defaultValue="Charlene Reed" />
+						<Input label="Email" defaultValue="charlenereed@gmail.com" />
+						<Input label="Password" type="password" defaultValue="**********" />
+						<Input
+							label="Present Address"
+							defaultValue="San Jose, California, USA"
+						/>
+						<Input
+							label="Date of Birth"
+							type="date"
+							defaultValue="1990-01-25"
+						/>
+						<Input
+							label="Permanent Address"
+							defaultValue="San Jose, California, USA"
+						/>
+						<Input label="City" defaultValue="San Jose" />
+						<Input label="Postal Code" defaultValue="45962" />
+						<Input label="Country" defaultValue="USA" />
+					</div>
+					<div className="page__content__form__buttons">
+						<button type="button">Save</button>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
